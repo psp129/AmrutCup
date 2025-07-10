e0fb57fe97e366cf44cc70ce91180682c4d0d9f5
Prachi Patel <prachipatel@Prachis-MacBook-Air.local>
2025-07-10 17:04:23 -0400

    Fix render

--- js/server.js
+++ js/server.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 const crypto = require('crypto');
 
-const serviceAccount = require('../serviceAccountKey.json');
+//const serviceAccount = require('../serviceAccountKey.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
